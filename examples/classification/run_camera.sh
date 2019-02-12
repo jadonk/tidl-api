81c66b6b510f69bf06e6e86a332c96faa7210079
Jason Kridner <jdk@ti.com>
2019-02-12 06:53:27 +0000

    Playing with format

--- examples/classification/run_camera.sh
+++ examples/classification/run_camera.sh
@@ -8,12 +8,13 @@ export DISPLAY=:0
 export XAUTHORITY=/home/debian/.Xauthority
 gedit &
 GEDIT_PID=$!
+cpufreq-set -f 1G
 ./tidl_classification -g 1 -d 2 -l ./imagenet.txt -s ./classlist.txt -i 0 -c ./stream_config_j11_v2.txt 1> /tmp/tidl.log 2> /tmp/tidl-err.log &
 TIDL_PID=$!
 echo $TIDL_PID > /var/run/tidl-demo.pid
-sleep 5
-wmctrl -r "Imagenet_EVEx0_DSPx2" -e 0,0,0,-1,-1
-wmctrl -r "Classlist" -e 0,600,0,-1,-1
-wmctrl -r "TIDL SW Stack" -e 0,575,200,-1,-1
+sleep 10
+wmctrl -r "Imagenet_EVEx0_DSPx2" -e 0,20,10,-1,-1
+wmctrl -r "Classlist" -e 0,550,10,-1,-1
+wmctrl -r "TIDL SW Stack" -e 0,550,210,-1,-1
 #wmctrl -r "Untitled Document 1 - gedit" -b add,hidden
 kill $GEDIT_PID
