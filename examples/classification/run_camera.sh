a2b2cb7fcb80b58638a7bdc6c01d34ad80a9b91d
Jason Kridner <jdk@ti.com>
2019-02-12 05:03:27 +0000

    Update demo for BeagleBone AI setup

--- examples/classification/run_camera.sh
+++ examples/classification/run_camera.sh
@@ -8,12 +8,12 @@ export DISPLAY=:0
 export XAUTHORITY=/home/debian/.Xauthority
 gedit &
 GEDIT_PID=$!
-./tidl_classification -g 1 -d 2 -e 2 -l ./imagenet.txt -s ./classlist.txt -i 0 -c ./stream_config_j11_v2.txt 1> /tmp/tidl.log 2> /tmp/tidl-err.log &
+./tidl_classification -g 1 -d 2 -l ./imagenet.txt -s ./classlist.txt -i 0 -c ./stream_config_j11_v2.txt 1> /tmp/tidl.log 2> /tmp/tidl-err.log &
 TIDL_PID=$!
 echo $TIDL_PID > /var/run/tidl-demo.pid
 sleep 5
-wmctrl -r "Imagenet_EVEx2_DSPx2" -e 0,0,0,-1,-1
-wmctrl -r "Classlist" -e 0,500,0,-1,-1
-wmctrl -r "TIDL SW Stack" -e 0,475,200,-1,-1
+wmctrl -r "Imagenet_EVEx0_DSPx2" -e 0,0,0,-1,-1
+wmctrl -r "Classlist" -e 0,600,0,-1,-1
+wmctrl -r "TIDL SW Stack" -e 0,575,200,-1,-1
 #wmctrl -r "Untitled Document 1 - gedit" -b add,hidden
 kill $GEDIT_PID
