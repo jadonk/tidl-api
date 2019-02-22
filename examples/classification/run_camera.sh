c085367e6f0249e44eaf1ca2017f5e43e3d7f30f
Jason Kridner <jdk@ti.com>
2019-02-22 20:27:41 +0000

    Enable EVEs, fix resolution and add thermal status display

--- examples/classification/run_camera.sh
+++ examples/classification/run_camera.sh
@@ -6,13 +6,14 @@ fi
 cd $(dirname $0)
 export DISPLAY=:0
 export XAUTHORITY=/home/debian/.Xauthority
+xrandr --output HDMI1 --mode 1024x768
 gedit &
 GEDIT_PID=$!
-./tidl_classification -g 1 -d 2 -l ./imagenet.txt -s ./classlist.txt -i 0 -c ./stream_config_j11_v2.txt 1> /tmp/tidl.log 2> /tmp/tidl-err.log &
+./tidl_classification -g 1 -d 2 -e 2 -l ./imagenet.txt -s ./classlist.txt -i 0 -c ./stream_config_j11_v2.txt 1> /tmp/tidl.log 2> /tmp/tidl-err.log &
 TIDL_PID=$!
 echo $TIDL_PID > /var/run/tidl-demo.pid
-sleep 10
-wmctrl -r "Imagenet_EVEx0_DSPx2" -e 0,20,10,-1,-1
+sleep 12
+wmctrl -r "Imagenet_EVEx2_DSPx2" -e 0,20,10,-1,-1
 wmctrl -r "Classlist" -e 0,550,10,-1,-1
 wmctrl -r "TIDL SW Stack" -e 0,550,210,-1,-1
 #wmctrl -r "Untitled Document 1 - gedit" -b add,hidden
--- examples/classification/thermal_led.sh
+++ examples/classification/thermal_led.sh
@@ -3,6 +3,8 @@ if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root" 
 	exit 1
 fi
+export DISPLAY=:0
+export XAUTHORITY=/home/debian/.Xauthority
 cpufreq-set -f 1G
 echo 0 > /sys/devices/system/cpu/cpu1/online
 echo oneshot > /sys/class/leds/beaglebone:green:usr4/trigger
@@ -10,5 +12,5 @@ echo 1 > /sys/class/leds/beaglebone:green:usr4/delay_off
 while true; do
 	echo "(`cat /sys/class/thermal/thermal_zone0/temp`-15000)/75" | bc > /sys/class/leds/beaglebone:green:usr4/delay_on
 	echo 1 > /sys/class/leds/beaglebone:green:usr4/shot
-	sleep 1
+	osd_cat /sys/class/thermal/thermal_zone0/temp -p bottom -d 1
 done
